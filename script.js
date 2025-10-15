4e4b9c260d452e790206b641c169894f15372dec
Naji2402 <najisinan2402@gmail.com>
2025-10-15 09:46:01 +0530

    brand filtering created

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 let productSection = document.querySelector('#productSection');
 let brandFilter = document.querySelectorAll('.filter-options');
-// let brands = document.querySelector('.brands');
-
-// let arrowRotate = document.querySelector('#hamburger-arrow');
 
 
 const productCart = [
@@ -199,11 +196,33 @@ const productCart = [
             "A15 Bionic Chip, 6 Core Processor Processor",
             "1 year warranty for phone and 1 year warranty for in Box Accessories"
         ] 
+    },
+    {
+        brandName: "Apple",
+        name: "Apple iPhone 16 (Black, 128 Gb)",
+        image: "./images/iphone1.webp",
+        finalRating: 4.6,
+        ratings: 136634,
+        reviews: 4114,
+        price: 104900,
+        realPrice:109900, 
+        discountPercentage: 5,
+        exchangeOfferPrice: 48650,
+        features: [
+            "128 GB ROM",
+            "15.49 cm (6.1 inch) Super Retina XDR Display",
+            "48MP + 12MP | 12MP Front Camera",
+            "A18 Chip, 6 Core Processor Processor",
+            "1 year warranty for phone and 1 year warranty for in Box Accessories"
+        ] 
     }
 ]
 
-productCart.forEach((product) => {
 
+
+
+productCart.forEach((product) => {
+    
     const getFeatures = product.features.map((feature) => {
         return `<li>${feature}</li>`;
     }).join('');
@@ -261,7 +280,6 @@ productCart.forEach((product) => {
 
 const brandSection = document.querySelectorAll('.brand-section');
 
-                   
 brandSection.forEach((section) => {
    let filterOption = section.querySelector('.filter-options');
    let brands = section.querySelector('.brands');
@@ -272,3 +290,84 @@ brandSection.forEach((section) => {
     arrowRotate.classList.toggle('rotate')
    });
 });
+
+
+
+
+let checkbox = document.querySelectorAll('.brand-check');
+
+checkbox.forEach((check) => {
+    check.addEventListener('click', () => {
+    productSection.innerHTML = '';
+    let filterdArray = productCart.filter((card) => {
+        if (check.checked && check.value === card.brandName) {
+           return card;
+        }
+    })
+    filterdArray.forEach((newCard) => {
+
+        const getFilteredFeatures = newCard.features.map((featu) => {
+            return `<li>${featu}</li>`;
+        }).join('');
+        productSection.innerHTML += `<div class="product">
+                        <div class="product-details-section">
+                            <div class="product-image">
+                                <div class="product-main-img">
+                                    <img src="${newCard.image}" alt="">
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="N1bADF" width="16" height="16" viewBox="0 0 20 16"><path d="M8.695 16.682C4.06 12.382 1 9.536 1 6.065 1 3.219 3.178 1 5.95 1c1.566 0 3.069.746 4.05 1.915C10.981 1.745 12.484 1 14.05 1 16.822 1 19 3.22 19 6.065c0 3.471-3.06 6.316-7.695 10.617L10 17.897l-1.305-1.215z" fill="#C2C2C2" class="x1UMqG" stroke="#FFFFFF" fill-rule="evenodd" opacity=".9"></path></svg>
+                                </div>
+                                <div class="add-to-compare-main">
+                                    <input type="checkbox" id="addToCompare" class="add-to-compater">
+                                    <label for="addToCompare">Add to compare</label>
+                                </div>
+                            </div>
+                            <div class="product-details">
+                                <div class="product-name">
+                                    ${newCard.name}
+                                </div>
+                                <div class="product-ratings-reviews">
+                                    <div class="product-rating">
+                                        <span>${newCard.finalRating}</span>
+                                        <img src="logos/star.svg" alt="">
+                                    </div>
+                                    <div class="product-review">
+                                        <span>${newCard.ratings} Ratings & ${newCard.reviews} Reviews</span>
+                                    </div>
+                                </div>
+                                <div class="product-features">
+                                    <ul>
+                                            ${getFilteredFeatures}
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="product-price-section">
+                            <div class="product-price">
+                                <span>₹${newCard.price}</span>
+                                <img src="logos/assured.png" alt="">
+                            </div>
+                            <div class="product-discount">
+                                <span>₹${newCard.realPrice}</span>
+                                <span>${newCard.discountPercentage}% off</span>
+                            </div>
+                            <div class="products-left">
+                                Only 1 left
+                            </div>
+                            <div class="product-exchange-offer">
+                                Upto <strong>₹${newCard.exchangeOfferPrice}</strong> off on Exchange
+                            </div>
+                        </div>
+                    </div>`
+    });   
+});
+});
+
+
+
+
+
+
+
+
+
+
